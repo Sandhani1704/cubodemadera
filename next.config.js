c51d0d294a19ba8cfd83eabe8d161f149768026b
Anna Galkina <sandhani1704@yandex.ru>
2022-06-02 11:04:08 +0300

    change build folder

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@
 //       disableStaticImages: true
 //     }
 //   }
-const withImages = require('next-images')
-module.exports = withImages()
+// const withImages = require('next-images')
+// module.exports = withImages()
 
-module.exports = {
-    images: {
-      disableStaticImages: true
-    }
-  }
+// module.exports = {
+//     images: {
+//       disableStaticImages: true
+//     }
+//   }
